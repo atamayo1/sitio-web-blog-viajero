ec680303b764568396c4666e593c72dca25bcde7
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-09-29 18:53:27 -0500

    Initial commit

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+<footer>
+
+</footer>
+
+<!-- Main Dev Script -->
+<script src="assets/js/maindev.js" ></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php include 'header.php'; ?>
+
+<?php require 'sections/section1.php'; ?>
+<?php require 'sections/section2.php'; ?>
+<?php require 'sections/section3.php'; ?>
+<?php require 'sections/section4.php'; ?>
+
+<?php require 'footer.php'; ?>
