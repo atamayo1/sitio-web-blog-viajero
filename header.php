2a021b5eb91a23d556300cd5a6cd667fa7ded32b
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-09-29 23:03:09 -0500

    realice la maquetación del menú en el header

--- header.php
+++ header.php
@@ -7,7 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog Viajero</title>
     <link rel="icon" href="assets/img/icono.jpg">
-
     <!-- Bootstrap CSS3 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <!-- Fuentes -->
@@ -21,9 +20,59 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <!-- Bootstrap Script -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/fed9e4ca1e.js" crossorigin="anonymous"></script>
 </head>
 <body>
 
-    <header>
-
+    <header class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <!--Logo-->
+                <div class="col-8 pt-2">
+                    <a href="index.php">
+                        <img class="logotipo img-fluid" src="assets/img/logotipo-negativo.png" alt="logo de juanito travel">
+                    </a>
+                </div>
+                <!--Redes Sociales-->
+                <div class="col-2 redes d-none d-lg-block">
+                    <ul class="d-flex justify-content-end pt-2 mt-1">
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-facebook-square lead rounded-circle text-white mr-1 text-center"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-instagram lead rounded-circle text-white mr-1 text-center"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-twitter lead rounded-circle text-white mr-1 text-center"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-youtube lead rounded-circle text-white mr-1 text-center"></i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" target="_blank">
+                                <i class="fab fa-snapchat-ghost lead rounded-circle text-white mr-1 text-center"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!--Buscador y Menú-->
+                <div class="col-4 col-md-2 col-lg-2 col-xl-2 d-flex justify-content-end pt-2 pt-md-4 pt-lg-2 pt-xl-3">
+                    <div class="pr-5 pr-lg-5 mt-0 mt-md-1 mt-lg-2 mt-xl-2">
+                        <i class="fas fa-search load text-white text-center"></i>
+                    </div>
+                    <div class="pr-2 pr-lg-3">
+                        <i class="fas fa-bars load text-white text-center"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
     </header>
