2ed3d108eaea41b99b6d5dab7f9cb2f4d831728b
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-09-29 19:28:21 -0500

    acomodé el entorno para empezar a maquetar

--- header.php
+++ header.php
@@ -6,10 +6,12 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blog Viajero</title>
-    <link rel="icon" href="assets/img/icono.jpgs">
+    <link rel="icon" href="assets/img/icono.jpg">
 
     <!-- Bootstrap CSS3 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Chewy|Open+Sans&display=swap" rel="stylesheet">
     <!-- Main Dev -->
     <link rel="stylesheet" href="assets/css/maindev.css">
 
@@ -22,6 +24,6 @@
 </head>
 <body>
 
-<header>
+    <header>
 
-</header>
+    </header>
