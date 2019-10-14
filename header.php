d6297bef08e97e086b87fc01e3e2122b5ddedf19
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-10-13 23:35:19 -0500

    añadí con bootstrap el buscador para móvil

--- header.php
+++ header.php
@@ -76,3 +76,13 @@
             </div>
         </div>
     </header>
+
+    <!-- Buscador para Móvil -->
+    <div class="container d-block d-md-none py-3">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Buscar">
+            <div class="input-group-append">
+                <span class="input-group-text"><i class="fas fa-search"></i></span>
+            </div>
+        </div>
+    </div>
