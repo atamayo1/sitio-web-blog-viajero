7d5f066203f217ac8ad58c3b36cc57e385016214
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-10-19 16:47:33 -0500

    añadí el banner al header

--- index.php
+++ index.php
@@ -1,9 +1,44 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog Viajero</title>
+    <link rel="icon" href="assets/img/icono.jpg">
+    <!-- Bootstrap CSS3 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- Fuentes -->
+    <link href="https://fonts.googleapis.com/css?family=Chewy|Open+Sans&display=swap" rel="stylesheet">
+    <!-- JdSlider -->
+    <link rel="stylesheet" href="assets/scss/css/jquery.jdSlider.css">
+    <!-- Main Dev -->
+    <link rel="stylesheet" href="assets/css/maindev.css">
 
-<?php include 'header.php'; ?>
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- FontAwesome -->
+    <script src="https://kit.fontawesome.com/fed9e4ca1e.js" crossorigin="anonymous"></script>
+    <!-- Popper Script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <!-- Bootstrap Script -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- JdSlider -->
+    <script src="assets/js/plugins/jquery.jdSlider-latest.js"></script>
+</head>
+<body>
 
-<?php require 'sections/section1.php'; ?>
-<?php require 'sections/section2.php'; ?>
-<?php require 'sections/section3.php'; ?>
-<?php require 'sections/section4.php'; ?>
+<?php require 'header.php';?>
+
+<?php include 'sections/section1.php'; ?>
+<?php include 'sections/section2.php'; ?>
+<?php include 'sections/section3.php'; ?>
+<?php include 'sections/section4.php'; ?>
 
 <?php require 'footer.php'; ?>
+
+<!-- Main Dev Script -->
+<script src="assets/js/maindev.js" ></script>
+</body>
+</html>
---
+++ search.php
@@ -0,0 +1,9 @@
+<!-- Buscador para Móvil -->
+<div class="container d-block d-md-none py-3" id="search">
+    <div class="input-group input-group-sm mb-3">
+        <input type="text" class="form-control" placeholder="Buscar">
+        <div class="input-group-append">
+            <span class="input-group-text"><i class="fas fa-search"></i></span>
+        </div>
+    </div>
+</div>
