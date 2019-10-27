ee1004966ad79c5b85d4da600328023c0e17fdac
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2019-10-27 17:07:51 -0500

    añadí la maqueta de la sección 2 de las entradas del blog

--- sections/section2.php
+++ sections/section2.php
@@ -1,1 +1,144 @@
-<?php
+<!-- Contenido Blog -->
+<section id="section2" class="container-fluid bg-white">
+    <div class="container">
+        <div class="row">
+            <!-- Columna Izquierda -->
+            <div class="col-12 col-lg-9 p-0 pr-lg-5">
+                <!-- Artículo 1 -->
+               <div class="row">
+                   <div class="col-12 col-lg-5">
+                       <h4 class="d-block d-lg-none py-3 title-article">Type something here</h4>
+                       <div class="date d-block d-lg-none">30.09.2018</div>
+                       <img src="/assets/img/articulo01.png" alt="Lorem Ipsum is simply dummy" class="img-fluid" width="100%">
+                   </div>
+                   <div class="col-12 col-lg-7 introArticle">
+                       <h4 class="d-none d-lg-block title-article">Type something here</h4>
+                       <p class="my-2 my-lg-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                       <a href="#" class="float-right">Leer más</a>
+                       <div class="date d-none d-lg-block">30.09.2018</div>
+                   </div>
+               </div>
+                <hr style="border: 1px solid #79FF39;">
+                <!-- Artículo 2 -->
+                <div class="row">
+                    <div class="col-12 col-lg-5">
+                        <h4 class="d-block d-lg-none py-3 title-article">Type something here</h4>
+                        <div class="date d-block d-lg-none">30.09.2018</div>
+                        <img src="/assets/img/articulo02.png" alt="Lorem Ipsum is simply dummy" class="img-fluid" width="100%">
+                    </div>
+                    <div class="col-12 col-lg-7 introArticle">
+                        <h4 class="d-none d-lg-block title-article">Type something here</h4>
+                        <p class="my-2 my-lg-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <a href="#" class="float-right">Leer más</a>
+                        <div class="date d-none d-lg-block">30.09.2018</div>
+                    </div>
+                </div>
+                <hr style="border: 1px solid #79FF39;">
+                <!-- Publicidad -->
+                <div class="d-block d-lg-none">
+                    <img src="/assets/img/ad02.jpg" alt="" class="img-fluid" width="100%">
+                </div>
+                <!-- Artículo 3 -->
+                <div class="row">
+                    <div class="col-12 col-lg-5">
+                        <h4 class="d-block d-lg-none py-3 title-article">Type something here</h4>
+                        <div class="date d-block d-lg-none">30.09.2018</div>
+                        <img src="/assets/img/articulo03.png" alt="Lorem Ipsum is simply dummy" class="img-fluid" width="100%">
+                    </div>
+                    <div class="col-12 col-lg-7 introArticle">
+                        <h4 class="d-none d-lg-block title-article">Type something here</h4>
+                        <p class="my-2 my-lg-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <a href="#" class="float-right">Leer más</a>
+                        <div class="date d-none d-lg-block">30.09.2018</div>
+                    </div>
+                </div>
+                <hr style="border: 1px solid #79FF39;">
+                <!-- Artículo 4 -->
+                <div class="row">
+                    <div class="col-12 col-lg-5">
+                        <h4 class="d-block d-lg-none py-3 title-article">Type something here</h4>
+                        <div class="date d-block d-lg-none">30.09.2018</div>
+                        <img src="/assets/img/articulo04.png" alt="Lorem Ipsum is simply dummy" class="img-fluid" width="100%">
+                    </div>
+                    <div class="col-12 col-lg-7 introArticle">
+                        <h4 class="d-none d-lg-block title-article">Type something here</h4>
+                        <p class="my-2 my-lg-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <a href="#" class="float-right">Leer más</a>
+                        <div class="date d-none d-lg-block">30.09.2018</div>
+                    </div>
+                </div>
+                <hr style="border: 1px solid #79FF39;">
+                <!-- Artículo 5 -->
+                <div class="row">
+                    <div class="col-12 col-lg-5">
+                        <h4 class="d-block d-lg-none py-3 title-article">Type something here</h4>
+                        <div class="date d-block d-lg-none">30.09.2018</div>
+                        <img src="/assets/img/articulo05.png" alt="Lorem Ipsum is simply dummy" class="img-fluid">
+                    </div>
+                    <div class="col-12 col-lg-7 introArticle">
+                        <h4 class="d-none d-lg-block title-article">Type something here</h4>
+                        <p class="my-2 my-lg-5">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+                        <a href="#" class="float-right">Leer más</a>
+                        <div class="date d-none d-lg-block">30.09.2018</div>
+                    </div>
+                </div>
+                <hr style="border: 1px solid #79FF39;">
+            </div>
+            <!-- Columna derecha -->
+            <div class="d-none d-lg-block col-lg-3 p-5">
+                <!-- Sobre mi -->
+                <div class="sobreMi">
+                    <h4>Sobre Mi</h4>
+                    <img src="/assets/img/sobreMi.jpg" alt="Lorem Ipsum is simply dummy text" class="img-fluid my-1">
+                    <p class="small">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                </div>
+                <!-- Publicidad -->
+                <div class="my-4">
+                    <img src="/assets/img/ad01.jpg" alt="" class="img-fluid">
+                </div>
+                <!-- Comentarios Recientes -->
+                <div class="my-4">
+                    <h4>Comentarios Recientes</h4>
+                    <div class="d-flex my-3">
+                        <div class="w-100 w-xl-50 pr-3 pt-2">
+                            <img src="/assets/img/com01.jpg" alt="Lorem Ipsum is simply dummy text" class="img-fluid">
+                        </div>
+                        <div>
+                            <p class="small">Lorem Ipsum is simply dummy text of the printing</p>
+                        </div>
+                    </div>
+                    <div class="d-flex my-3">
+                        <div class="w-100 w-xl-50 pr-3 pt-2">
+                            <img src="/assets/img/com02.jpg" alt="Lorem Ipsum is simply dummy text" class="img-fluid">
+                        </div>
+                        <div>
+                            <p class="small">Lorem Ipsum is simply dummy text of the printing</p>
+                        </div>
+                    </div>
+                    <div class="d-flex my-3">
+                        <div class="w-100 w-xl-50 pr-3 pt-2">
+                            <img src="/assets/img/com03.jpg" alt="Lorem Ipsum is simply dummy text" class="img-fluid">
+                        </div>
+                        <div>
+                            <p class="small">Lorem Ipsum is simply dummy text of the printing</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Publicidad -->
+                <div class="ads my-4">
+                    <img src="/assets/img/ad02.jpg" alt="" class="img-fluid">
+                </div>
+                <!-- Publicidad -->
+                <div class="ads my-4">
+                    <img src="/assets/img/ad05.png" alt="" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
